87d8438f9ab289b63bfa62655fcd84ba5fd459a8
huangchao <106504007+3289006168@users.noreply.github.com>
2022-10-26 21:20:01 +0800

    test2-commit-commit

--- test-8003/src/main/java/com/atguigu/controller/test.java
+++ test-8003/src/main/java/com/atguigu/controller/test.java
@@ -10,6 +10,7 @@ public class test {
     @GetMapping("/test/TimeOut")
     public String Test() throws InterruptedException {
         TimeUnit.SECONDS.sleep(5);
+        System.out.println("test-1");
         System.out.println("test");
         System.out.println("test2-commit");
         System.out.println("test3-commit");
